8eadcbb07c9886743f4a0653bceeca7fe03e8ece
wowleo74 <wowleo74@gmail.com>
2026-03-17 20:48:11 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,40 @@
-const cacheName="molegame-v1"
+const cacheName = "molegame-v2"; // 버전을 v2로 올려서 새 코드를 반영합니다.
 
-const filesToCache=[
-"/",
-"/index.html",
-"/sieun.png",
-"/gaeun.png",
-"/doll.png",
-"/dad.png"
-]
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sieun.png",
+  "./gaeun.png",
+  "./doll.png",
+  "./dad.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(cacheName)
-.then(cache=>cache.addAll(filesToCache))
-)
-})
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .then(() => self.skipWaiting()) // 설치 즉시 활성화
+  );
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request)
-.then(response=>response || fetch(e.request))
-)
-})
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key); // 이전 버전 캐시 삭제
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  );
+});
